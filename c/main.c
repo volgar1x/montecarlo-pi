48d2c64f8dfb70b4deaaee881ec80af2bf2b1d1b
Blackrush <blackrushx@gmail.com>
2015-02-12 12:38:16 +0100

    Fix whitespaces

--- c/main.c
+++ c/main.c
@@ -6,39 +6,39 @@
 static double dd = (double) RAND_MAX + 1;
 
 double rand_rd(unsigned int *seed) {
-	int val = rand_r(seed);
-	return val / dd;
+  int val = rand_r(seed);
+  return val / dd;
 }
 
 int sample(int n) {
-	unsigned int seed = time(NULL);
+  unsigned int seed = time(NULL);
 
-	int i;
-	int inside;
-	double x, y, z;
+  int i;
+  int inside;
+  double x, y, z;
 
-	inside = 0;
-	for (i = 0; i < n; i++) {
-		x = rand_rd(&seed);
-		y = rand_rd(&seed);
+  inside = 0;
+  for (i = 0; i < n; i++) {
+    x = rand_rd(&seed);
+    y = rand_rd(&seed);
 
-		z = x*x+y*y;
+    z = x*x+y*y;
 
-		if (z < 1.0) {
-			inside++;
+    if (z < 1.0) {
+      inside++;
     }
   }
 
-	return inside;
+  return inside;
 }
 
 int main(int argc, char **argv) {
 
-	int n = 1000000000;
-	int result = sample(n);
-	double pi = (double) result / (double) n * 4.0;
+  int n = 1000000000;
+  int result = sample(n);
+  double pi = (double) result / (double) n * 4.0;
 
-	printf("%f\n", pi);
+  printf("%f\n", pi);
 
-	return 0;
+  return 0;
 }
